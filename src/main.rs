7925a25cb5e629bcb0221d30822d6a6b4477ffce
Tim Adams <adamstimb@gmail.com>
2025-07-16 17:18:38 +0200

    docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+//!# Mandelbrot CLI
+//!
+//!_A Rust-powered Mandelbrot Set generator._
+//!
+//!## Installation
+//!
+//!Install Mandelbrot CLI with cargo:
+//!
+//!```bash
+//!$ cargo install mandelbrot_cli
+//!```
+//!
+//!## Usage
+//!
+//!Generate the Mandelbrot Set with default settings:
+//!
+//!```bash
+//!$ mandelbrot_cli
+//!```
+//!
+//!Open the output file "out.png".
+//!
+//!Explore the Mandelbrot Set and refine your images using the available options:
+//!
+//!```
+//!  -s, --size <SIZE>                    [default: 2160]
+//!  -x, --x-offset <X_OFFSET>            [default: 0]
+//!  -y, --y-offset <Y_OFFSET>            [default: 0]
+//!  -m, --magnification <MAGNIFICATION>  [default: 1]
+//!  -i, --iterations <ITERATIONS>        [default: 100]
+//!  -o, --output-path <OUTPUT_PATH>      [default: out.png]
+//!  -h, --help                           Print help
+//!```
+//!
+//!**Negative numbers**: You can pass negative numbers to the -x and -y arguments like this:
+//!
+//!```
+//!$ mandelbrot_cli -x=-1.0 -y=-0.23
+//!```
+//!
+//!## Usage as a Crate
+//!
+//!The functions to rendering the Mandelbrot Set are public so you can import them into your own Rust projects.  See Crate documentation for details.
+//!
+//!
 use rayon::prelude::*;
 use core::panic;
 use std::time::Instant;
